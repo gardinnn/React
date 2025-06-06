6bf2eeaef3f688a7b7b7dccf3a5b908c69aa9f67
Gabriel gardin <gabrielgardin7@gmail.com>
2025-06-06 17:53:02 -0300

    render por if

--- aula02/src/App.js
+++ aula02/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
-import SayMyName from './components/SayMyName';
-import Pessoa from './components/pessoa';
-import List from './components/list';
-import Evento from './components/Evento';
-import Form from './components/Forms';
+// import SayMyName from './components/SayMyName';
+// import Pessoa from './components/pessoa';
+// import List from './components/list';
+// import Evento from './components/Evento';
+// import Form from './components/Forms';
+import Condicional from './components/condicional';
 
 
 function App() {
@@ -11,9 +12,11 @@ const nome = 'jesus'
 
   return (
     <div className="App">
-      <h1>Testando Eventos</h1>
-      <Evento/>
-      <Form/>
+      {/* <h1>Testando Eventos</h1> */}
+      {/* <Evento/>
+      <Form/> */}
+      <h1>Renderização condicional</h1>
+      <Condicional/>
     </div>
   );
 }
