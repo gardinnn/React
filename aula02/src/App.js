24dd0741063ca33377d077f3a903c1eaba23488e
Gabriel gardin <gabrielgardin7@gmail.com>
2025-06-09 15:53:07 -0300

    renderização de listas

--- aula02/src/App.js
+++ aula02/src/App.js
@@ -1,22 +1,27 @@
 import './App.css';
+import OutraLista from './components/OutraLista';
 // import SayMyName from './components/SayMyName';
 // import Pessoa from './components/pessoa';
 // import List from './components/list';
 // import Evento from './components/Evento';
 // import Form from './components/Forms';
-import Condicional from './components/condicional';
+// import Condicional from './components/condicional';
 
 
 function App() {
 const nome = 'jesus'
+
+const meusItens = ['react', 'vue', 'angular']
 
   return (
     <div className="App">
       {/* <h1>Testando Eventos</h1> */}
       {/* <Evento/>
       <Form/> */}
-      <h1>Renderização condicional</h1>
-      <Condicional/>
+      <h1>Renderização de Listas</h1>
+      {/* <Condicional/> */}
+      <OutraLista itens={meusItens}/>
+      <OutraLista itens={[]}/>
     </div>
   );
 }
