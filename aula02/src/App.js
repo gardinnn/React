6f155e3b21a25916783630675ecaeed486027f2f
Gabriel gardin <gabrielgardin7@gmail.com>
2025-06-16 17:25:36 -0300

    react router

--- aula02/src/App.js
+++ aula02/src/App.js
@@ -1,7 +1,7 @@
-import './App.css';
-import { useState } from 'react';
-import SeuNome from './components/SeuNome';
-import Saudacao from './components/saudacao';
+// import './App.css';
+// import { useState } from 'react';
+// import SeuNome from './components/SeuNome';
+// import Saudacao from './components/saudacao';
 // import OutraLista from './components/OutraLista';
 // import SayMyName from './components/SayMyName';
 // import Pessoa from './components/pessoa';
@@ -9,26 +9,50 @@ import Saudacao from './components/saudacao';
 // import Evento from './components/Evento';
 // import Form from './components/Forms';
 // import Condicional from './components/condicional';
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom'
+import Home from './pages/Home';
+import Empresa from './pages/Empresa';
+import Contato from './pages/Contato';
+import NavBar from './layout/NavBar';
+import Footer from './layout/footer';
 
 
 function App() {
-  const [nome, setNome] = useState()
+  // const [nome, setNome] = useState()
 // const nome = 'jesus'
 
 // const meusItens = ['react', 'vue', 'angular']
 
   return (
-    <div className="App">
-      {/* <h1>Testando Eventos</h1> */}
-      {/* <Evento/>
-      <Form/> */}
-      <h1>State Lift</h1>
-      {/* <Condicional/> */}
-      {/* <OutraLista itens={meusItens}/>
-      <OutraLista itens={[]}/> */}
-      <SeuNome setNome={setNome}/>
-      <Saudacao nome = {nome}/>
-    </div>
+    // <div className="App">
+      /* <h1>Testando Eventos</h1> */
+      /* <Evento/>
+      <Form/> */
+      /* <h1>State Lift</h1> */
+      /* <Condicional/> */
+      /* <OutraLista itens={meusItens}/>
+      <OutraLista itens={[]}/> */
+      /* <SeuNome setNome={setNome}/>
+      <Saudacao nome = {nome}/> */
+    // </div>
+    
+      <Router>
+        <NavBar/>
+        <Switch>
+          <Route exact path= "/">
+            <Home/>
+          </Route>
+          <Route path= "/empresa">
+            <Empresa/>
+          </Route>
+          <Route path= "/contato">
+            <Contato/>
+          </Route>
+        </Switch>
+        <Footer/>
+      </Router>
+      
+    
   );
 }
 
